0d7c1100284356d46bac128beb9fa6bd9bf7e927
iqbal-devgit config --global user.name Your <pkmdiqbal@gmail.com>
2023-09-29 01:34:54 +0600

    [update - token

--- src/lib/token/index.js
+++ src/lib/token/index.js
@@ -1,10 +1,22 @@
 const { serverError } = require("../../utils/error");
 const jwt = require("jsonwebtoken");
 
+/**
+ * Generate a JSON Web Token (JWT) based on the provided payload.
+ *
+ * @param {object} params - Parameters for generating the JWT.
+ * @param {object} params.payload - The payload to be included in the JWT.
+ * @param {string} [params.algorithm="HS256"] - The signing algorithm to use.
+ * @param {string} [params.secret] - The secret key for signing the JWT (default taken from environment variables).
+ * @param {string} [params.expiresIn="1h"] - The expiration time for the JWT.
+ * @returns {string} The generated JWT.
+ * @throws {Error} If there is an issue generating the token.
+ */
+
 const generateToken = ({
   payload,
   algorithm = "HS256",
-  secret = process.env.ACCESS_TOKEN_SECRET,
+  secret = process.env.ACCESS_TOKEN_SECRET ?? "qwuierqwegrugqwehrjv",
   expiresIn = "1h",
 }) => {
   try {
@@ -13,7 +25,6 @@ const generateToken = ({
       expiresIn,
     });
   } catch (e) {
-    console.log("[JWT]", e);
     throw serverError();
   }
 };
