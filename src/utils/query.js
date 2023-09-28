225a4a77c7b52ecf5aeb84b6d8c96a0963609dbc
iqbal-devgit config --global user.name Your <pkmdiqbal@gmail.com>
2023-09-28 11:21:31 +0600

    Add comments to improve code readability in getTransformedItems, getHATEOASForAllItems, and getPagination functions

--- src/utils/query.js
+++ src/utils/query.js
@@ -1,6 +1,15 @@
 const defaults = require("../config/defaults");
 const { generateQueryString } = require("./qs");
 
+/**
+ * Get pagination information based on the provided parameters.
+ *
+ * @param {object} params - The pagination parameters.
+ * @param {number} [params.totalItems] - The total number of items.
+ * @param {number} [params.limit] - The number of items per page.
+ * @param {number} [params.page] - The current page number.
+ * @returns {object} Pagination information.
+ */
 const getPagination = ({
   totalItems = defaults.totalItems,
   limit = defaults.limit,
@@ -25,6 +34,19 @@ const getPagination = ({
   return pagination;
 };
 
+/**
+ * Generate HATEOAS (Hypermedia as the Engine of Application State) links for a paginated list of items.
+ *
+ * @param {object} params - The parameters for generating HATEOAS links.
+ * @param {string} [params.url="/"] - The base URL.
+ * @param {string} [params.path=""] - The path for the current resource.
+ * @param {object} [params.query={}] - The query parameters for the current resource.
+ * @param {boolean} [params.hasNext=false] - Indicates if there is a next page.
+ * @param {boolean} [params.hasPrev=false] - Indicates if there is a previous page.
+ * @param {number} [params.page=1] - The current page number.
+ * @returns {object} HATEOAS links for the current resource.
+ */
+
 const getHATEOASForAllItems = ({
   url = "/",
   path = "",
@@ -48,7 +70,16 @@ const getHATEOASForAllItems = ({
 
   return links;
 };
-
+/**
+ * Transform an array of items by selecting specific properties and adding a link to each item.
+ *
+ * @param {object} params - The parameters for transforming items.
+ * @param {Array} [params.items=[]] - The array of items to be transformed.
+ * @param {Array} [params.selection=[]] - An array of property names to be selected from each item.
+ * @param {string} [params.path="/"] - The base path for generating item links.
+ * @returns {Array} Transformed items with selected properties and links.
+ * @throws {Error} If `items` or `selection` is not an array.
+ */
 const getTransformedItems = ({ items = [], selection = [], path = "/" }) => {
   if (!Array.isArray(items) || !Array.isArray(selection)) {
     throw new Error("Invalid selection");
